c0b2554a3f98180e696d48736de753e7511fcd5d
iphy <31126819+iphytian@users.noreply.github.com>
2022-11-22 14:26:28 +0800

    Update node_status_refresh.py

--- node_status_refresh.py
+++ node_status_refresh.py
@@ -3,6 +3,7 @@
 # 进行node的staus更新
 
 from py2neo import Graph,Node,Relationship,NodeMatcher
+import time
 
 # 使用nodematcher找到节点
 # Use nodematcher to find nodes
@@ -26,7 +27,7 @@ def read_csv(file_name):
     return final_list
 
 graph = Graph('http://localhost:7474',user='neo4j',password='19830722')
-
+time_start = time.time()
 data = read_csv('C:/Users/iphy/Desktop/updata_parameter.csv')
 
 for i in range(len(data)):
@@ -37,3 +38,38 @@ for i in range(len(data)):
         match_data.update({'status1': data[i][1]})
         match_data.update({'status2': data[i][2]})
         graph.push(match_data) # push()方法将更新后的节点压入图中 The push() method pushes the updated node into the graph
+
+# 提取参数特征并转化为特征向量
+# Extract the node parameter and merge to feature vectors
+b = graph.run("MATCH (n{classes:'enviroment'})"
+              "MATCH p=({name:n.name})-[*]->({name:$name}) RETURN nodes(p)",name = 'Stick_like image')
+new = b.data()
+data = []
+for i in range(len(new)):
+    new1 = new[i]['nodes(p)']
+    data1 = []
+    print(i)
+    print(new1)
+    for j in range(len(new1)):
+        data1.append(new1[j]['status1'])
+        data1.append(new1[j]['status2'])
+    data.append(data1)
+
+c = graph.run("MATCH (n{classes:'device'})"
+              "MATCH p=({name:n.name})-[*]->({name:$name}) RETURN nodes(p)",name = 'Stick_like image')
+new = c.data()
+print(new)
+for i in range(len(new)):
+    new1 = new[i]["nodes(p)"]
+    print(i)
+    print(new1)
+    data1 = []
+    for j in range(len(new1)):
+        data1.append(new1[j]['status1'])
+        data1.append(new1[j]['status2'])
+    data.append(data1)
+print(data)
+
+time_end = time.time()
+time_sum = time_end - time_start
+print(time_sum)
