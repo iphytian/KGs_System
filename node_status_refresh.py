c21ac39b22a32fbaf69000da7b3601a99820ad30
iphy <31126819+iphytian@users.noreply.github.com>
2022-11-23 10:47:40 +0800

    Update node_status_refresh.py

--- node_status_refresh.py
+++ node_status_refresh.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# 进行node的staus更新
+# 进行node的staus更新,查询并给出feature vector
 
 from py2neo import Graph,Node,Relationship,NodeMatcher
-import time
 
 # 使用nodematcher找到节点
 # Use nodematcher to find nodes
@@ -26,50 +25,69 @@ def read_csv(file_name):
             final_list.append(row.split(','))
     return final_list
 
-graph = Graph('http://localhost:7474',user='neo4j',password='19830722')
-time_start = time.time()
-data = read_csv('C:/Users/iphy/Desktop/updata_parameter.csv')
+def update():
+    graph = Graph('http://localhost:7474',user='neo4j',password='19830722')
+    data = read_csv('C:/Users/iphy/Desktop/updata_parameter.csv')
 
-for i in range(len(data)):
-    match_data = findNode(data[i][0], graph)
-    if match_data ==None:
-        break
+    for i in range(len(data)):
+        match_data = findNode(data[i][0], graph)
+        if match_data ==None:
+            break
+        else:
+            match_data.update({'status1': data[i][1]})
+            match_data.update({'status2': data[i][2]})
+            graph.push(match_data) # push()方法将更新后的节点压入图中 The push() method pushes the updated node into the graph
+
+#定义匹配结果
+#define the query result
+def find_image_quality(data0):
+    image_quality = ''
+    if data0 == 0:
+        image_quality = 'no_bright_star image'
+    elif data0 == 1:
+         print('telescope is changing survey area')
+    elif data0 == 2:
+        image_quality = 'Stick_like image'
+    elif data0 == 3:
+        image_quality = 'Donut_like image'
+    elif data0 == 4:
+        image_quality = 'Two_point_like image'
+    elif data0 == 5:
+        print('telescope is normal')
     else:
-        match_data.update({'status1': data[i][1]})
-        match_data.update({'status2': data[i][2]})
-        graph.push(match_data) # push()方法将更新后的节点压入图中 The push() method pushes the updated node into the graph
+        image_quality = 'Lumpy_like image'
+    return image_quality
 
-# 提取参数特征并转化为特征向量
-# Extract the node parameter and merge to feature vectors
-b = graph.run("MATCH (n{classes:'enviroment'})"
-              "MATCH p=({name:n.name})-[*]->({name:$name}) RETURN nodes(p)",name = 'Stick_like image')
-new = b.data()
-data = []
-for i in range(len(new)):
-    new1 = new[i]['nodes(p)']
-    data1 = []
-    print(i)
-    print(new1)
-    for j in range(len(new1)):
-        data1.append(new1[j]['status1'])
-        data1.append(new1[j]['status2'])
-    data.append(data1)
+def merge_feature(core_phase):
+    graph = Graph('http://localhost:7474', user='neo4j', password='19830722')
+    a0 = find_image_quality(core_phase)
+    # 提取参数特征并转化为特征向量
+    # Extract the node parameter and merge to feature vectors
+    b = graph.run("MATCH (n{classes:'enviroment'})"
+                  "MATCH p=({name:n.name})-[*]->({name:$name}) RETURN nodes(p)",name = a0)
+    new = b.data()
+    data = []
+    for i in range(len(new)):
+        new1 = new[i]['nodes(p)']
+        data1 = []
+        print(i)
+        print(new1)
+        for j in range(len(new1)):
+            data1.append(new1[j]['status1'])
+            data1.append(new1[j]['status2'])
+        data.append(data1)
 
-c = graph.run("MATCH (n{classes:'device'})"
-              "MATCH p=({name:n.name})-[*]->({name:$name}) RETURN nodes(p)",name = 'Stick_like image')
-new = c.data()
-print(new)
-for i in range(len(new)):
-    new1 = new[i]["nodes(p)"]
-    print(i)
-    print(new1)
-    data1 = []
-    for j in range(len(new1)):
-        data1.append(new1[j]['status1'])
-        data1.append(new1[j]['status2'])
-    data.append(data1)
-print(data)
-
-time_end = time.time()
-time_sum = time_end - time_start
-print(time_sum)
+    c = graph.run("MATCH (n{classes:'device'})"
+                  "MATCH p=({name:n.name})-[*]->({name:$name}) RETURN nodes(p)",name = a0)
+    new = c.data()
+    print(new)
+    for i in range(len(new)):
+        new1 = new[i]["nodes(p)"]
+        print(i)
+        print(new1)
+        data1 = []
+        for j in range(len(new1)):
+            data1.append(new1[j]['status1'])
+            data1.append(new1[j]['status2'])
+        data.append(data1)
+    return data
